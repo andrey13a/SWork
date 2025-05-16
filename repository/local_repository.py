f8acf456ab79c20c41311772e1a28a47d82c0717
andrey13a <49250855+andrey13a@users.noreply.github.com>
2025-05-15 21:47:24 -0300

    Database connection
    Adição do banco de dados sqlite ao repository

--- repository/local_repository.py
+++ repository/local_repository.py
@@ -1,13 +1,33 @@
 # base class for data persistence
+import sqlite3
+from entity.local_entity import Local
 
 
 class LocalRepository:
 
-    def __init__(self):
-        self.locals = []
-    
-    def save(self, local):
-        self.locals.append(local)
+    atributos = ["Id", "Nome", "Responsavel"]
+
+    def __init__(self, db_name="database.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.create_table()
+
+    def create_table(self):
+        with self.conn:
+            self.conn.execute("""CREATE TABLE IF NOT EXISTS local_table(
+                              id INTEGER PRIMARY KEY,
+                              nome text,
+                              responsavel text
+                              )""")
+
+    def save(self, local_obra):
+        with self.conn:
+            self.conn.execute("INSERT INTO local_table values(?,?,?)",(
+                              local_obra.id,
+                              local_obra.nome,
+                              local_obra.responsavel))
     
     def get_all(self):
-        return self.locals
+        with self.conn:
+            return [Local(id, nome, responsavel) for id, nome, responsavel
+                    in self.conn.execute("SELECT * FROM local_table")]
+    
--- repository/tool_repository.py
+++ repository/tool_repository.py
@@ -1,16 +1,40 @@
 # Classe usada para permanencia das Ferramentas
+import sqlite3
+from entity.tool_entity import Tool
 
 class ToolRepository:
 
-    def __init__(self):
-        self.tools = []
-        self.tipos = ["furadeira", "esmerilhadeira", "maquina de solda"]
-        self.marcas = ["dewalt", "bosch", "arcweld"]
-        self.atributos = ("id", "tipo", "tensao", "marca", "modelo", "condicao")
+    atributos = ["Id", "Tipo", "Tensao", "Marca", "Modelo", "Condicao"]
 
-    
+    def __init__(self, db_name="database.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.create_table()
+
+    def create_table(self):
+        with self.conn:
+            self.conn.execute("""CREATE TABLE IF NOT EXISTS tools_table(
+                             id INTEGER PRIMARY KEY,
+                             tipo text,
+                             tensao text,
+                             marca text,
+                             modelo text,
+                             condicao text
+                             )"""
+                              )
+
     def save(self, tool):
-        self.tools.append(tool)
-    
+        with self.conn:
+            self.conn.execute("INSERT INTO tools_table values(?,?,?,?,?,?)",(
+                                tool.id, 
+                                tool.tipo, 
+                                tool.tensao, 
+                                tool.marca, 
+                                tool.modelo, 
+                                tool.condicao
+                               )
+                               )
+
     def get_all(self):
-        return self.tools
+        with self.conn:
+            return [Tool(id, tipo, tensao, marca, modelo, condicao) for id, tipo, tensao, marca, modelo, condicao 
+                    in self.conn.cursor().execute("SELECT * FROM tools_table")]
